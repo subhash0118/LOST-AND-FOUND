3967f87d08ba245ab813d0b7a61e42d0685d7478
subhash0118 <ksubhash11812@gmail.com>
2025-04-01 15:54:54 +0530

    first-commit

--- kk/src/pages/Home.jsx
+++ kk/src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
           FIND YOUR LOST ITEMS HERE!!
         </p>
         <Link to="/login">
-          <button className="px-6 md:px-8 py-2 md:py-3 border-2 border-black hover:bg-black hover:text-white transition">
+          <button className="px-6 md:px-8 py-2 md:py-3 border-2 border-black hover:bg-black hover:text-white transition cursor-pointer">
             GET STARTED
           </button>
         </Link>
